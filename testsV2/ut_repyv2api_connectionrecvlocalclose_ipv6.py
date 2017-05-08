ac5ad5d68642e1c7768159dac34af950a61eebfb
ankitbhatia32 <ankitbhatia1413@gmail.com>
2017-05-08 01:45:05 -0400

    Initial ipv6 unit test added

---
+++ testsV2/ut_repyv2api_connectionrecvlocalclose_ipv6.py
@@ -0,0 +1,30 @@
+"""
+Check what happens with send and close
+"""
+#pragma repy restrictions.twoports
+
+localip = "::1"
+localport = 12345
+targetip = "::1"
+targetport = 12346
+timeout = 1.0
+
+
+tcpserversocket = listenforconnection_ipv6(targetip, targetport)
+
+conn = openconnection_ipv6(targetip, targetport, localip, localport, timeout)
+
+
+(ip, port, serverconn) = tcpserversocket.getconnection()
+
+assert(ip == localip)
+assert(port == localport)
+
+conn.close()
+try:
+  data = conn.recv(10)
+except SocketClosedLocal:
+  pass
+else:
+  log("Should get an error if we closed the socket locally",'\n')
+
